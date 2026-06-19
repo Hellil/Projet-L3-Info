d95f9763bfffb7074412e406dbf1144d508c3571
dolphy <adolphe.ESSONGUE-MOUNYENGUILA@etu.univ-amu.fr>
2026-06-19 14:44:11 +0200

    EnemySimple un enemy simple qui patrouille en infligeant des dégats normalement, PlayerHealth

---
+++ Projet/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log($"[PLAYER] Aïe ! Vie restante : {currentHealth}");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("[PLAYER] Le joueur est mort !");
+        //ajouter l'écran de game over ou qq chose pour dire que le joueur est mort 
+    }
+}
